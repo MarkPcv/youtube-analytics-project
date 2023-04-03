c992c9dc13ea9a38e2527176edce62f9393fb39e
Mark Pochivalov <pochivalov.mark@gmail.com>
2023-04-03 17:42:31 +0300

    Modify channel.py functions: __init__() and print_info()

--- src/channel.py
+++ src/channel.py
@@ -1,12 +1,24 @@
+import json
+import os
+from googleapiclient.discovery import build
 
 
 class Channel:
     """Класс для ютуб-канала"""
+    # Define API key for YouTube account
+    api_key = os.getenv('YT_API_KEY')
+    # Create special object to work with API
+    youtube = build('youtube', 'v3', developerKey=api_key)
 
     def __init__(self, channel_id: str) -> None:
-        """Экземпляр инициализируется id канала. Дальше все данные будут подтягиваться по API."""
-        pass
+        """
+        Экземпляр инициализируется id канала.
+        Дальше все данные будут подтягиваться по API.
+        """
+        self.channel_id = channel_id
+        self.channel = Channel.youtube.channels().\
+            list(id=channel_id, part='snippet,statistics').execute()
 
     def print_info(self) -> None:
-        """Выводит в консоль информацию о канале."""
-        pass
+        """Return dictionary in json-like comfortable format with indents"""
+        print(json.dumps(self.channel, indent=2, ensure_ascii=False))
