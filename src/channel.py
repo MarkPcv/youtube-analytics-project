d0adba21847ed91a1917f1f71291ea13e5698183
Mark Pochivalov <pochivalov.mark@gmail.com>
2023-04-06 10:29:12 +0300

    Update channel.py: add instance attributes and getter for "channel_id"

--- src/channel.py
+++ src/channel.py
@@ -15,10 +15,31 @@ class Channel:
         Экземпляр инициализируется id канала.
         Дальше все данные будут подтягиваться по API.
         """
-        self.channel_id = channel_id
-        self.channel = Channel.youtube.channels().\
+        # Retrieve information about channel as JSON object
+        self.channel = Channel.youtube.channels(). \
             list(id=channel_id, part='snippet,statistics').execute()
+        # Retrieve channel id
+        self.__channel_id = channel_id
+        # Retrieve channel title
+        self.title = self.channel['items'][0]['snippet']['title']
+        # Retrieve channel description
+        self.description = self.channel['items'][0]['snippet']['description']
+        # Retrieve channel URL
+        self.url = "https://www.youtube.com/channel/" + channel_id
+        # Retrieve number of subscribers
+        self.subs_count = self. \
+            channel['items'][0]['statistics']['subscriberCount']
+        # Retrieve total number of videos
+        self.video_count = self. \
+            channel['items'][0]['statistics']['videoCount']
+        # Retrieve total number of videos
+        self.view_count = self. \
+            channel['items'][0]['statistics']['viewCount']
 
     def print_info(self) -> None:
         """Return dictionary in json-like comfortable format with indents"""
         print(json.dumps(self.channel, indent=2, ensure_ascii=False))
+
+    @property
+    def channel_id(self):
+        return self.__channel_id
